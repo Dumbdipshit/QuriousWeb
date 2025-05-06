760f46e9f47bb9b80f48db3561a142ea08f323f9
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-05-06 14:35:10 +0200

    Update
    added a favicon and made a logo and name

--- src/index.php
+++ src/index.php
@@ -5,13 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HomePage</title>
     <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" type="image/x-icon" href="assets/images/magnify glass_blue.png">
 </head>
 <body>
+<<<<<<< Updated upstream
 
     <?php
         echo("test echo");
     ?>
 
+=======
+    <header></header>
+    <main></main>
+    <footer></footer>
+>>>>>>> Stashed changes
     <script src="scripts/script.js"></script>
 </body>
 </html>
