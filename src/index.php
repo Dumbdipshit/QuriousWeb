6ce43b7e065a58e36b9f493411d5e9cc666a9aab
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-05-19 11:04:32 +0200

    Update
    -Added a seperation for the info and discription. This will make it easier for future stuff

--- src/index.php
+++ src/index.php
@@ -56,23 +56,27 @@
                  <div class="website-items-container">
                     <!-- This is the image for the locations and can be changed -->
                     <img class="items-image" src="assets/images/city.png" alt="city">
-                    <div class="website-items-text-container">
-                        <p class="website-items-travel-to-text">
-                            Travel to:
-                        </p>
+                        <div class="website-name-and-info-column-container">
+                            <div class="website-items-text-container">
+                                <p class="website-items-travel-to-text">
+                                    Travel to:
+                                </p>
 
-                        <!-- This is the location name and the "Location" name can be changed -->
-                        <h1 class="website-items-name-text">
-                            New Eridu
-                        </h1>
-                        <p class="website-items-text">
-                            A place in: <span class="website-items-country-text">America<span>
-                        </p>
-                        <!-- This is the discription of the location. It can be changed and wont be the same. The maximun lines should be 4 lines -->
-                        <p class="website-items-description-text">
-                            This is a fictional place from the game Zenless Zone Zero. I took the image because it looked nice and it has alot of pixels.
-                        </p>
-                    </div>
+                                <!-- This is the location name and the "Location" name can be changed -->
+                                <h1 class="website-items-name-text">
+                                    New Eridu
+                                </h1>
+                                <p class="website-items-text">
+                                    A place in: <span class="website-items-country-text">America<span>
+                                </p>
+                            </div>
+                            <div class="website-items-text-container test">
+                                    <!-- This is the discription of the location. It can be changed and wont be the same. The maximun lines should be 4 lines -->
+                                    <p class="website-items-description-text">
+                                        This is a fictional place from the game Zenless Zone Zero. I took the image because it looked nice and it has alot of pixels.
+                                    </p>
+                            </div>
+                        </div>
                  </div>
 
             </div>
