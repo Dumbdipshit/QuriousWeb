fc6007e1507ad9d26916db1105c071797e49f84f
Nick-v-d-v <1210727@student.roc-nijmegen.nl>
2025-06-03 10:24:18 +0200

    header
    de header is af en ben nu bezig met de footer

---
+++ src/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+    <body>
+    <div class="header">
+    <div onclick="ShowMenu()" class="header-more-options-knop">
+      <div class="more-options-knop">
+        <img src="assets/images/more options.png" alt="more options">
+      </div>
+    </div>
+    <div class="logo"><img src="assets/images/logo.png" alt="Logo"></div>
+    <input type="text" placeholder="" class="zoekbalk">
+    <div class="magnify-glass"><img src="assets/images/magnify glass_darkBlue.png" alt="magnify glass"></div>
+  </div>
+
+  <div class="container-more-options">
+    <div class="gray-box"><img src="assets/images/house.png" alt="Home"></div>
+    <div class="gray-box"><img src="assets/images/location.png" alt="Location"></div>
+    <div class="gray-box"><img src="assets/images/about us.png" alt="Group"></div>
+    <div class="gray-box"><img src="assets/images/contact_blue.png" alt="Telefoon"></div>
+    <div class="gray-box"><img src="assets/images/acount.png" alt="Emoji"></div>
+  </div>
+</body>
+
+<script src="script.js"></script>
+
+</html>
