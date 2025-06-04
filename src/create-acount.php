f09351417fc9839874f47e29a46ee51279396611
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-06-04 09:58:21 +0200

    Update
    -I forgot to that this branch didnt have a create acount because i was working on the wrong branch

---
+++ src/create-acount.php
@@ -0,0 +1,71 @@
+<?php
+    session_start();   
+    include ("connect.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flight</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" type="image/x-icon" href="assets/images/magnify glass_darkBlue.png">
+</head>
+<body>
+    <header>
+        <?php
+            include ("header.php");
+        ?>
+    </header>
+
+    <main>
+        <div class="website-main-column-container">
+            <h1 class="website-location-light-text margin-top">
+                Nice to meet you. Lets make a acount to know you better.
+            </h1>
+            <!-- This is the container of creating a acount -->
+            <form class="website-location-container light-gray-outline medium-width" action="process-created-acount.php" method="post">
+                <div class="website-create-content-form">
+                    <p class="smaller-text warning-text">
+                        <?php
+                            if($_SESSION["error"] == "DuplicateUsername"){
+                                echo "The username is already taken";
+                                $_SESSION["error"] = "none";
+                            }elseif($_SESSION["error"] == "UnMatchingPassword"){
+                                echo "The confirmed password has to be the same as your password";
+                                $_SESSION["error"] = "none";
+                            }
+                        ?>
+                    </p>
+
+                    <label class="website-location-light-text smaller-text" for="username">Username:</label>
+                    <input class="website-input-style smaller-text" type="text" autocomplete="off" name="username">
+
+                    <label class="website-location-light-text smaller-text" for="password">Password:</label>
+                    <input class="website-input-style smaller-text" type="text" autocomplete="off" name="password">
+
+                    <label class="website-location-light-text smaller-text" for="password">Confirm Password:</label>
+                    <input class="website-input-style smaller-text" type="text" autocomplete="off" name="confirmPassword">
+
+                    <label class="website-location-light-text smaller-text" for="password">Your pet name:</label>
+                    <input class="website-input-style smaller-text" type="text" autocomplete="off" name="petName">
+
+
+
+                    <div class="padding-bottom">
+                        <input class="website-blue-button" type="submit" value="create"></input>
+                    </div>
+                </div> 
+            </form>
+        </div>
+    </main>
+
+    <footer>
+        <?php
+            include ("footer.php");
+        ?>
+    </footer>
+    <script src="scripts/script.js"></script>
+</body>
+</html>
