81cd63e9fd044f5c6af5e0560a923e1cdf25e707
Nick-v-d-v <1210727@student.roc-nijmegen.nl>
2025-06-05 11:29:58 +0200

    Footer
    ik heb nu de footer af gemaakt

--- src/footer.php
+++ src/footer.php
@@ -9,10 +9,33 @@
 </body>
   <div class="container-footer">
     <div class="contact-us">
-      <div class="contact-us!">
-          
+      <div class="contact-us-text">
+          <p>Contact us!</p>
+          <div class="contact"><img src="assets/images/contact_blue.png" alt="contact"></div>
       </div>
     </div>
+      <div class="stay-qurious">
+        <p>Stay Qurious</p>
+        <div class="small-home"><img src="assets/images/house.png" alt="contact"></div>
+        <div class="small-home-text">
+          <p>Home</p>
+        </div>
+        <div class="small-location"><img src="assets/images/location.png" alt="contact"></div>
+        <div class="small-location-text">
+          <p>Discover</p>
+        </div>
+        <div class="small-about-us"><img src="assets/images/about us.png" alt="contact"></div>
+        <div class="small-about-us-text">
+          <p>About us</p>
+        </div>
+      </div>
+      <div class="instagram"><img src="assets/images/instagram blue.png" alt="contact"></div>
+      <div class="twitter-blue"><img src="assets/images/twitter blue.png" alt="contact"></div>
+      <div class="small-logo"><img src="assets/images/logo.png" alt="contact"></div>
+      <div class="back-to-top">
+        <P>Back to top</P>
+      </div>
   </div>
 </body>
 </html>
+
