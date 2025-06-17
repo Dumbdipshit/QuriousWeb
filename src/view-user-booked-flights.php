dd9e24c41b62d7d8b4f64ee291091ea4aed97f38
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-06-17 13:10:37 +0200

    Update
    -Added the visuals of how the page should be shown. Still have to add php code

--- src/view-user-booked-flights.php
+++ src/view-user-booked-flights.php
@@ -31,9 +31,33 @@
     <main>
         <!-- This is the flex column container for the all the items in the website -->
         <div class="website-main-column-container">
-            <div class="website-location-row-container smaller-width">
-                <h1 class="website-location-light-text">Booked Flights:</h1>
-            </div>
+                <h1 class="website-location-light-text align-self-center">Booked Flights:</h1>
+                
+                <div class="website-location-container light-gray-outline">
+                    <div class="website-review-row-container smaller-gap">
+                        <div class="website-profile-box">
+                            <!-- This is the profile image the image can be changed -->
+                            <img class="profile-image" src="assets/images/city.png" alt=":)">
+                        </div>
+                        <!-- The username can be changed -->
+                        <h1 class="username-text">
+                            Username
+                        </h1>
+                    </div>
+
+                    <div class="website-row-container padding-bottom">
+                        <!-- The Location  can be changed -->
+                        <p class="website-location-light-text">
+                            Booked: <span>Location</span>
+                        </p>
+
+                        <form class="view-button-location" action="" method="post">
+                            <input class="hide-content" type="number" name="flightId" value="0">
+                            <input class="website-blue-button" type="submit" value="View">
+                        </form>
+                    </div>
+
+                </div>
 
         </div>
     </main>
