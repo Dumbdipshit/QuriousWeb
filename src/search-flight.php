7af2bb023c22f313c0e5d64f64b6bc42e6a9d62e
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-06-17 23:14:03 +0200

    Update
    -added the ability to search flights

---
+++ src/search-flight.php
@@ -0,0 +1,82 @@
+<?php
+    session_start();   
+    include ("connect.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" type="image/x-icon" href="assets/images/magnify glass_darkBlue.png">
+</head>
+<body onload="LoadScript()">
+    <?php
+        $db = $conn->prepare("
+            SELECT * FROM Flights
+            WHERE name  LIKE '%". $_POST["search"] ."%';
+        ");
+
+        $db->execute();
+        $result = $db->fetchAll();
+    ?>
+
+    <header>
+        <?php
+            include ("header.php");
+        ?>
+    </header>
+
+    <main>
+
+        <!-- This is the flex column container for the all the items in the website -->
+        <div class="website-main-column-container">
+            <!-- This is the container of the travel options for the webiste -->
+            <div class="website-items-wrap-container">
+                <?php
+                    for($i  = 0; $i < count($result); $i++){
+                ?>
+                    <!-- This is the visuals for the content of the website -->
+                    <a class="website-items-container" href='location-page.php?id=<?php echo $result[$i]['id']; ?>'>
+                        <!-- This is the image for the locations and can be changed -->
+                        <img class="items-image" src="assets/images/city.png" alt="city">
+                            <div class="website-name-and-info-column-container">
+                                <div class="website-items-text-container">
+                                    <p class="website-items-travel-to-text">
+                                        Travel to:
+                                    </p>
+
+                                    <!-- This is the location name and the "Location" name can be changed -->
+                                    <h1 class="website-items-name-text">
+                                        <?php echo $result[$i]['name'];?>
+                                    </h1>
+                                    <p class="website-items-text">
+                                        <!-- The County text can be changed -->
+                                        A place in: <span class="website-items-country-text"><?php echo $result[$i]['country'];?><span>
+                                    </p>
+                                </div>
+                                <div class="website-items-text-description-container">
+                                        <!-- This is the discription of the location. It can be changed and wont be the same. -->
+                                        <p class="website-items-description-text website-display-text">
+                                            <?php echo $result[$i]['description'];?>
+                                        </p>
+                                </div>
+                            </div>
+                    </a>
+                <?php
+                    }
+                ?>
+            </div>
+        </div>
+    </main>
+    
+    <footer>
+        <?php
+            include ("footer.php");
+        ?>
+    </footer>
+    <script src="scripts/script.js"></script>
+</body>
+</html>
