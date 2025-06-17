7112f623d59d152b2298446b937122bb291d9d34
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-06-17 12:11:03 +0200

    Update
    -Now when you delete a flight you will also delete the reviews with it

--- src/view-user-booked-flights.php
+++ src/view-user-booked-flights.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HomePage</title>
+    <title>View booked flights</title>
     <link rel="stylesheet" href="css/main.css">
     <link rel="icon" type="image/x-icon" href="assets/images/magnify glass_darkBlue.png">
 </head>
@@ -34,7 +34,7 @@
             <div class="website-location-row-container smaller-width">
                 <h1 class="website-location-light-text">Booked Flights:</h1>
             </div>
-            
+
         </div>
     </main>
     
